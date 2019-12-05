2c3adcd1e1204ce5186cb5a2cb3719d1cecb3fac
Harbes <jerryshen68@hotmail.com>
2019-12-05 11:33:29 +0800

    read from SQLserver and transfer to pandas.DataFrame

--- Lab-ReadFromOracle.py
+++ Lab-ReadFromOracle.py
@@ -1,5 +1,7 @@
 import cx_Oracle
 import pandas as pd
+
+# Barra风险数据
 user,password='riskdata','riskdata'
 dsn=cx_Oracle.makedsn('172.16.100.188','1522','markdb')
 def ConnectOracle(user,password,dsn):
@@ -11,3 +13,9 @@ connect=ConnectOracle(user,password,dsn)
 sql_order="select * from BARRA_FACTORRET"
 df=pd.read_sql(sql_order,connect);df.head()
 
+# 组合数据
+user,password='risk_read','riskRead2019' # risk_read 密码：riskRead2019
+dsn=cx_Oracle.makedsn('172.16.100.230','1521','xrisk')
+connect=ConnectOracle(user,password,dsn)
+sql_order="select * from all_tab_comments"
+df=pd.read_sql(sql_order,connect);df.head()
