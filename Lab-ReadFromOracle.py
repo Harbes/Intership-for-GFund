90c222d4cd5e89aa62660f1ca84bf75874d91ddc
Harbes <jerryshen68@hotmail.com>
2019-12-05 13:07:47 +0800

    daily updates

--- Lab-ReadFromOracle.py
+++ Lab-ReadFromOracle.py
@@ -14,8 +14,9 @@ sql_order="select * from BARRA_FACTORRET"
 df=pd.read_sql(sql_order,connect);df.head()
 
 # 组合数据
-user,password='risk_read','riskRead2019' # risk_read 密码：riskRead2019
+user,password='risk_read','riskRead2019'
 dsn=cx_Oracle.makedsn('172.16.100.230','1521','xrisk')
 connect=ConnectOracle(user,password,dsn)
-sql_order="select * from all_tab_comments"
-df=pd.read_sql(sql_order,connect);df.head()
+sql_order="select * from xrisk.tcrp_hld"
+trcp_hold=pd.read_sql(sql_order,connect);df.head()
+trcp_hold.to_pickle('trcp_hld')
